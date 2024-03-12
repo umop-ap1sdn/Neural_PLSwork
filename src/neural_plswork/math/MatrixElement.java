e912f917071b138979586bda17e7ee36612565ee
Caleb Devon <cmdevon2003@gmail.com>
2024-03-12 02:19:06 -0500

    Added basic javadoc comments to MatrixElement

--- src/neural_plswork/math/MatrixElement.java
+++ src/neural_plswork/math/MatrixElement.java
@@ -1,6 +1,24 @@
 package neural_plswork.math;
 
+/**
+ * Much of the math to be done in neural networks involves Matrix operations.
+ * This interface is used to add a layer of abstraction to matrices, allowing all elements of this interface
+ * to be used within Matrices by defining a Multiply and Add instruction for them
+ */
+
 public interface MatrixElement<T> {
+
+    /**
+     * Function to multiply 2 objects. May be implemented in different ways per implemented class
+     * @param other Other object to be multiplied with
+     * @return Object result of multiplication
+     */
     public T multiply(T other);
+
+    /**
+     * Function to add 2 objects. May be implemented in different ways per implemented class
+     * @param other Other object to be add with
+     * @return Object result of addition
+     */
     public T add(T other);
 }
