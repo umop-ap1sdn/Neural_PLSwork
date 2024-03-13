b4202df221afd8296234f5eb3a4a037f69214b8c
Caleb Devon <cmdevon2003@gmail.com>
2024-03-13 15:41:58 -0500

    Create NetworkValue object

---
+++ src/neural_plswork/core/NetworkValue.java
@@ -0,0 +1,37 @@
+package neural_plswork.core;
+
+import neural_plswork.math.MatrixElement;
+
+public class NetworkValue implements MatrixElement {
+    protected double value = 0.0;
+
+    protected NetworkValue() {
+        value = 0.0;
+    }
+
+    protected NetworkValue(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public MatrixElement add(MatrixElement other) {
+        if(!(other instanceof NetworkValue)) return other.add(this);
+        return new NetworkValue(value + ((NetworkValue)other).value);
+    }
+
+    @Override
+    public MatrixElement multiply(MatrixElement other) {
+        if(!(other instanceof NetworkValue)) return other.multiply(this);
+        return new NetworkValue(value * ((NetworkValue)other).value);
+    }
+
+    @Override
+    public MatrixElement negate() {
+        return new NetworkValue(-1 * value);
+    }
+
+    @Override
+    public MatrixElement copy() {
+        return new NetworkValue(value);
+    }
+}
