5c869825721656f35699d49987118d5f60491133
Caleb Devon <cmdevon2003@gmail.com>
2024-03-13 19:42:16 -0500

    Add exception throw to identity matrix class

--- src/neural_plswork/math/IdentityMatrix.java
+++ src/neural_plswork/math/IdentityMatrix.java
@@ -5,12 +5,20 @@ import neural_plswork.math.exceptions.IllegalMatrixException;
 
 public class IdentityMatrix extends Matrix<ConstantElement> {
     
+    final int size;
+
     public IdentityMatrix(int size) throws IllegalMatrixException {
         super(Matrix.buildIdentityMatrix(size).matrix);
+        this.size = size;
         
     }
 
     public <T extends MatrixElement> Vector<T> multiply(Vector<T> other) throws IllegalMatrixException {
-        return other.copy();
+        if(other.getRows() != size) throw new IllegalMatrixException("Matrices are incompatible for multiplication");
+        return (Vector<T>) other.copy();
+    }
+
+    public int getSize() {
+        return size;
     }
 }
