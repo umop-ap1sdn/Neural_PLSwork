27bd3d55aa21f3250888b09fa2811206adac8fe5
Caleb Devon <cmdevon2003@gmail.com>
2024-03-18 00:54:04 -0500

    Create incompatibility exception class

---
+++ src/neural_plswork/math/exceptions/ElementIncompatibleException.java
@@ -0,0 +1,8 @@
+package neural_plswork.math.exceptions;
+
+public class ElementIncompatibleException extends Exception {
+
+    public ElementIncompatibleException(String message) {
+        super(message);
+    }
+}
