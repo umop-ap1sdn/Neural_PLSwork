f09528c8fdd4c7485ebf7b0261557807ca6328d8
Caleb Devon <cmdevon2003@gmail.com>
2024-03-19 13:36:00 -0500

    Begin implementing NeuronLayer

--- src/neural_plswork/core/NeuronLayer.java
+++ src/neural_plswork/core/NeuronLayer.java
@@ -1,5 +1,21 @@
 package neural_plswork.core;
 
+import java.util.LinkedList;
+
+import neural_plswork.activations.ActivationFunction;
+import neural_plswork.math.Vector;
+
+@SuppressWarnings("unused")
 public abstract class NeuronLayer {
     
+    private LinkedList<Vector<NetworkValue>> unactivated;
+    private LinkedList<Vector<NetworkValue>> activated;
+    private LinkedList<Vector<NetworkValue>> derivative;
+    private LinkedList<Vector<NetworkValue>> error;
+
+    private ActivationFunction activation;
+
+    private int layerSize;
+    private int historySize;
+    private boolean bias;
 }
