7d5c9d670e91abe142bb21551f27068ab9080c2a
Caleb Devon <cmdevon2003@gmail.com>
2024-03-27 17:24:43 -0500

    Begin creating optimization functions

---
+++ src/neural_plswork/optimizer/OptimizationFunction.java
@@ -0,0 +1,8 @@
+package neural_plswork.optimizer;
+
+import neural_plswork.core.NetworkValue;
+import neural_plswork.math.Matrix;
+
+public interface OptimizationFunction {
+    public Matrix<NetworkValue> computeDeltas(Matrix<NetworkValue> gradients, double learning_rate);
+}
---
+++ src/neural_plswork/optimizer/StochasticGradientDescent.java
@@ -0,0 +1,13 @@
+package neural_plswork.optimizer;
+
+import neural_plswork.core.NetworkValue;
+import neural_plswork.math.Matrix;
+
+public class StochasticGradientDescent implements OptimizationFunction {
+
+	@Override
+	public Matrix<NetworkValue> computeDeltas(Matrix<NetworkValue> gradients, double learning_rate) {
+		return gradients.scale(new NetworkValue(-1 * learning_rate));
+	}
+    
+}
