d353095ff2469044d2c53f345ea964effac27a90
Caleb Devon <cmdevon2003@gmail.com>
2024-03-27 10:17:56 -0500

    Begin implementing optimizers

---
+++ src/neural_plswork/optimizer/InvalidOptimizerException.java
@@ -0,0 +1,11 @@
+package neural_plswork.optimizer;
+
+public class InvalidOptimizerException extends RuntimeException {
+    protected InvalidOptimizerException(String message) {
+        super(message);
+    }
+
+    protected InvalidOptimizerException() {
+        super("Invalid Optimizer Function");
+    }
+}
