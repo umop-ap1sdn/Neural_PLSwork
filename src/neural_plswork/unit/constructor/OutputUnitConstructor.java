a9915e28cd17de7da54148abc3036894daabeb7f
Caleb Devon <cmdevon2003@gmail.com>
2024-04-03 12:23:37 -0500

    Create OutputUnitConstructor

--- src/neural_plswork/unit/constructor/OutputUnitConstructor.java
+++ src/neural_plswork/unit/constructor/OutputUnitConstructor.java
@@ -3,25 +3,33 @@ package neural_plswork.unit.constructor;
 import neural_plswork.activations.ActivationFunction;
 import neural_plswork.core.ConnectionLayer;
 import neural_plswork.core.NeuronLayer;
+import neural_plswork.evaluation.Differentiable;
 import neural_plswork.initialize.Initializer;
+import neural_plswork.layers.basic.OutputLayer;
 import neural_plswork.network.InvalidNetworkConstructionException;
 import neural_plswork.optimizer.OptimizationFunction;
 import neural_plswork.regularization.penalize.Penalty;
-import neural_plswork.unit.ffUnits.HiddenUnit;
+import neural_plswork.unit.ffUnits.OutputUnit;
 
-public class HiddenUnitConstructor implements UnitConstructor {
+public class OutputUnitConstructor implements UnitConstructor {
+
+    private Differentiable eval;
+
+    public OutputUnitConstructor(Differentiable eval) {
+        this.eval = eval;
+    }
 
     @Override
-    public HiddenUnit construct(NeuronLayer[] prior, ActivationFunction[] activation, int[] layerSize, int historyLength,
+    public OutputUnit construct(NeuronLayer[] prior, ActivationFunction[] activation, int[] layerSize, int historyLength,
             boolean[] bias, int MAX_THREADS, Initializer[] initializer, Penalty[] penalty,
             OptimizationFunction[] optimizer) throws InvalidNetworkConstructionException {
         // HiddenUnit constructor
-        if(activation.length != 1 || layerSize.length != 1 || bias.length != 1) 
-            throw new InvalidNetworkConstructionException("HiddenUnit can only have 1 layer");
+        if(activation.length != 1 || layerSize.length != 1 || bias.length != 0) 
+            throw new InvalidNetworkConstructionException("OutputUnit can only have 1 layer and no bias");
         if(initializer.length != prior.length && optimizer.length != prior.length || penalty.length != prior.length)
             throw new InvalidNetworkConstructionException("ConnectionLayer parameters must be equal to number of ConnectionLayers");
         
-        NeuronLayer hiddenLayer = new NeuronLayer(activation[0], layerSize[0], historyLength, bias[0], MAX_THREADS);
+        OutputLayer hiddenLayer = new OutputLayer(eval, activation[0], layerSize[0], historyLength, MAX_THREADS);
 
         ConnectionLayer[] cLayers = new ConnectionLayer[prior.length];
 
@@ -29,7 +37,7 @@ public class HiddenUnitConstructor implements UnitConstructor {
             cLayers[i] = new ConnectionLayer(prior[i], hiddenLayer, initializer[i], optimizer[i], penalty[i]);
         }
 
-        return new HiddenUnit(hiddenLayer, cLayers, historyLength);
+        return new OutputUnit(hiddenLayer, cLayers, historyLength);
     }
     
 }
