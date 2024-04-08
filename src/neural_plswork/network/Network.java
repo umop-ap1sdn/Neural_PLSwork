ac06447b196b82b11de3935c20de38d1595e4a96
Caleb Devon <cmdevon2003@gmail.com>
2024-04-08 10:06:30 -0500

    Add test function to network class

--- src/neural_plswork/network/Network.java
+++ src/neural_plswork/network/Network.java
@@ -1,9 +1,12 @@
 package neural_plswork.network;
 
 import neural_plswork.connection.penalize.Penalty;
+import neural_plswork.core.NetworkValue;
 import neural_plswork.layers.basic.InputLayer;
+import neural_plswork.math.Vector;
 import neural_plswork.neuron.evaluation.Differentiable;
 import neural_plswork.neuron.evaluation.Evaluation;
+import neural_plswork.unit.Unit;
 import neural_plswork.unit.ffUnits.HiddenUnit;
 import neural_plswork.unit.ffUnits.OutputUnit;
 
@@ -23,5 +26,14 @@ public class Network {
 
         this.evaluator = evaluator;
         this.reporter = reporter;
+    }
+
+    public double[] test(double... inputs) {
+        Vector<NetworkValue> vector = NetworkValue.arrToVector(inputs);
+        input.setInputs(vector, 0);
+        for(Unit u: hidden) u.forwardPass(0);
+        output.forwardPass(0);
+
+        return NetworkValue.vectorToArr(output.getOutputs(0));
     }
 }
