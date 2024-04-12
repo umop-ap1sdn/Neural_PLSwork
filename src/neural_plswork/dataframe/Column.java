96c79cf09ab478cc3371ae621952b7c4f2bfd7a6
Caleb Devon <cmdevon2003@gmail.com>
2024-04-12 11:51:44 -0500

    Add additional functions to turn columns into subclasses

--- src/neural_plswork/dataframe/Column.java
+++ src/neural_plswork/dataframe/Column.java
@@ -78,9 +78,18 @@ public class Column<T> {
         return new Column<Double>(numbers);
     }
 
-    protected DoubleColumn getDoubleColumn() {
-        if(!(data instanceof Double[])) return null;
+    public DoubleColumn getDoubleColumn() {
+        if(!(data instanceof Double[])) throw new InvalidColumnException("Cannot turn this column into a double column");
         return new DoubleColumn((Double[]) data);
     }
     
+    public StringColumn geStringColumn() {
+        if(!(data instanceof String[])) throw new InvalidColumnException("Cannot turn this column into a String Column");
+        return new StringColumn((String[]) data);
+    }
+
+    public BooleanColumn getBooleanColumn() {
+        if(!(data instanceof Boolean[])) throw new InvalidColumnException("Cannot turn this column into a Boolean Column");
+        return new BooleanColumn((Boolean[]) data);
+    }
 }
---
+++ src/neural_plswork/dataframe/InvalidColumnException.java
@@ -0,0 +1,7 @@
+package neural_plswork.dataframe;
+
+public class InvalidColumnException extends RuntimeException {
+    protected InvalidColumnException(String message) {
+        super(message);
+    }
+}
